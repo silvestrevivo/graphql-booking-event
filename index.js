2c7ff39f176a71d4d81c72dc5f3922fbf433b06e
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-08 14:23:00 +0100

    basic server graphql configuration

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery{
+      events: [String!]!
+    }
+
+    type RootMutation{
+      createEvent(name: String): String!
+    }
+
+    schema{
+      query:RootQuery
+      mutation:RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Going out', 'chattering', 'taking a walk']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}))
 
 const port = process.env.PORT || 3000;
 
